9f5be92c169f8b4578b2458a1bc12aedf11b628d
SHAH RUKH MEHDI <srmehdi59@gmail.com>
2026-01-13 02:24:14 +0530

    added logout functionality and few bug fixes

--- src/app/features/header/header.ts
+++ src/app/features/header/header.ts
@@ -1,7 +1,8 @@
-import { Component, inject } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, inject, signal } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 import { State } from '../../core/services/state/state';
 import { Role } from '../../shared/utils/enums';
+import { Storage } from '../../core/services/storage/storage';
 
 @Component({
   selector: 'app-header',
@@ -14,8 +15,24 @@ export class Header {
   state = inject(State);
   user = this.state.user;
   roles: typeof Role = Role;
+  popupOpen = signal(false);
+  router = inject(Router);
+  storage = inject(Storage);
 
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
+  }
+  togglePopup() {
+    this.popupOpen.update((v) => !v);
+  }
+
+  closePopup() {
+    this.popupOpen.set(false);
+  }
+
+  logout() {
+    this.storage.clearStorage();
+    this.closePopup();
+    this.router.navigate(['/login']);
   }
 }
